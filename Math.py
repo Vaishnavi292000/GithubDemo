f8d3da254957330ca2b2381410f3ad497a53fa9e
USER <vaishnavi.b.k.2000@gmail.com>
2021-07-02 14:52:15 +0530

    Implemented multiply() in Bug456 branch

--- Math.py
+++ Math.py
@@ -7,7 +7,7 @@ def subtract(x,y):
     return x-y              #on main branch
 # Multiply implementation
 def multiply(x,y):
-    pass
+    return x*y              #on Bug456 branch
 # Divide implementation
 def divide(x,y):
     pass 
